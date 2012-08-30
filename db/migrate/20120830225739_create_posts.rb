2d04b3c5180e5bf794b50140ec13325917c9fe21
Jason Kellie <jkellie@me.com>
2012-08-30 19:33:47 -0400

    add index to users and posts tables

--- db/migrate/20120830225739_create_posts.rb
+++ db/migrate/20120830225739_create_posts.rb
@@ -2,8 +2,11 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :url
+      t.integer :user_id
 
       t.timestamps
     end
+    
+    add_index(:posts, :user_id)
   end
 end
