80e2abe153c5ccd5cc6b85ab5a470b8989774332
Jason Kellie <jkellie@me.com>
2012-08-31 09:46:00 -0400

    add user_id to attr_accessible of post model and validate presence

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
-  attr_accessible :url
+  attr_accessible :url, :user_id
   
-  validates :url, presence: true
+  validates :url,     presence: true
+  validates :user_id, presence: true
 end
