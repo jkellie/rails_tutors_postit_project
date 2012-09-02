378ae69b00aa8c5b61f0c82bc4b8acd067c6d441
Jason Kellie <jkellie@me.com>
2012-09-01 22:10:19 -0400

    add validation for body, post_id, and user_id

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,9 @@
 class Comment < ActiveRecord::Base
   attr_accessible :body, :post_id, :user_id
+  belongs_to :user
+  belongs_to :posts
+  
+  validates :body,    presence: true
+  validates :post_id, presence: true
+  validates :user_id, presence: true
 end
