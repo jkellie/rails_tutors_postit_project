02781ce71e163bd78d29759686a746dfc28c4806
Jason Kellie <jkellie@me.com>
2012-09-01 22:09:35 -0400

    add comment associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :url, :user_id
   belongs_to :user
+  has_many :posts
   
   validates :url,     presence: true
   validates :user_id, presence: true
