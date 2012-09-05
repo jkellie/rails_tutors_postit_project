f24ef866f13776331790b58781c9deb43f379f0e
Jason Kellie <jkellie@me.com>
2012-09-05 12:06:40 -0400

    add title to post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120901003940) do
+ActiveRecord::Schema.define(:version => 20120905160307) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20120901003940) do
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "title"
   end
 
   add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
