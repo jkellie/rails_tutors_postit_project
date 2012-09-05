3896e567fc01f339db61ff3fa84295e220ede9bb
Jason Kellie <jkellie@me.com>
2012-09-05 19:32:00 -0400

    modify routes to place comments under posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :url, :user_id, :title
   belongs_to :user
-  has_many :posts
+  has_many :comments
   
   validates :url,     presence: true
   validates :user_id, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Postit::Application.routes.draw do
-  resources :comments
-
   root :to => 'posts#index'
   
   resources :users
   
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
+  
+  
 end
