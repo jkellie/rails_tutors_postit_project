36202eebe6696298fd6cc5b099aca3b941cabf10
Jason Kellie <jkellie@me.com>
2012-09-05 17:17:39 -0400

    add title to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,11 @@
 class Post < ActiveRecord::Base
-  attr_accessible :url, :user_id
+  attr_accessible :url, :user_id, :title
   belongs_to :user
   has_many :posts
   
   validates :url,     presence: true
   validates :user_id, presence: true
+  validates :title,   presence: true
   
   default_scope order: 'posts.created_at DESC'
 end
